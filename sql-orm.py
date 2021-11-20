4df787c08e363250a0d7cbf53acf6fe3781b0be4
Joanna Gorska <gorska919@gmail.com>
2021-11-20 21:45:57 +0000

    Add classess for each table in the database

--- sql-orm.py
+++ sql-orm.py
@@ -15,6 +15,36 @@ from sqlalchemy.orm import sessionmaker
 db = create_engine("postgresql:///chinook")
 base = declarative_base
 
+
+# create a class-based model for the "Artist" table
+class Artist(base):
+    __tablename__ = "Artist"
+    ArtistId = Column(Integer, primary_key=True)
+    Name = Column(String)
+
+
+# create a class-based model for the "Album" table
+class Album(base):
+    __tablename__ = "Album"
+    AlbumId = Column(Integer, primary_key=True)
+    Title = Column(String)
+    ArtistId = Column(Integer, ForeignKey("Artist.ArtistId"))
+
+
+# create a class-based model for the "Track" table
+class Track(base):
+    __tablename__ = "Track"
+    TrackId = Column(Integer, primary_key=True)
+    Name = Column(String)
+    AlbumId = Column(Integer, ForeignKey("Album.AlbumId"))
+    MediaTypeId = Column(Integer, primary_key=False)
+    GenreId = Column(Integer, primary_key=False)
+    Composer = Column(String)
+    Milliseconds = Column(Integer, primary_key=False)
+    Bytes = Column(Integer, primary_key=False)
+    UnitPrice = Column(Float)
+
+
 # session will create new instance of sessionmaker
 # than point to engine (db) instead of connect
 
