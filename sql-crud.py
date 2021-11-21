48cf31fe75c2b1f630aadceee2544545b0061dd7
Joanna Gorska <gorska919@gmail.com>
2021-11-21 18:01:18 +0000

    Add programers to the database and print the result to the console

--- sql-crud.py
+++ sql-crud.py
@@ -82,3 +82,26 @@ tim_berners_lee = Programmer(
     nationality="British",
     famous_for="World Wide Web"
 )
+# add each instance of our programmers to our session
+# session.add(ada_lovelace)
+# session.add(alan_turing)
+# session.add(grace_hopper)
+# session.add(margaret_hamilton)
+# session.add(bill_gates)
+# session.add(tim_berners_lee)
+
+# commit our session to the database
+# session.commit()
+
+
+# query the database to find all Programmers
+programmers = session.query(Programmer)
+for programmer in programmers:
+    print(
+        programmer.id,
+        programmer.first_name + " " + programmer.last_name,
+        programmer.gender,
+        programmer.nationality,
+        programmer.famous_for,
+        sep=" | "
+    )
