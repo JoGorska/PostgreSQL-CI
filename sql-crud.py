25932483b6625825d96d89bd0324d3a68817ce79
Joanna Gorska <gorska919@gmail.com>
2021-11-21 19:52:31 +0000

    Add code to delete multiple records

--- sql-crud.py
+++ sql-crud.py
@@ -125,22 +125,30 @@ gal_anonim = Programmer(
 
 
 # deleting a single record
-fname = input("Enter a first name: ")
-lname = input("Enter a last name: ")
-programmer = session.query(Programmer).filter_by(first_name=fname, last_name=lname).first()
+# fname = input("Enter a first name: ")
+# lname = input("Enter a last name: ")
+# programmer = session.query(Programmer).filter_by(first_name=fname, last_name=lname).first()
 
-# defensive programming
-if programmer is not None:
-    print("Programmer Found: ", programmer.first_name + " " + programmer.last_name)
-    confirmation = input("Are you sure you want to delete this record? (y/n) ")
-    if confirmation.lower() == "y":
-        session.delete(programmer)
-        session.commit()
-        print("Programmer has been deleted")
-    else:
-        print("Programmer not deleted")
-else:
-    print("No records found")
+# # defensive programming
+# if programmer is not None:
+#     print("Programmer Found: ", programmer.first_name + " " + programmer.last_name)
+#     confirmation = input("Are you sure you want to delete this record? (y/n) ")
+#     if confirmation.lower() == "y":
+#         session.delete(programmer)
+#         session.commit()
+#         print("Programmer has been deleted")
+#     else:
+#         print("Programmer not deleted")
+# else:
+#     print("No records found")
+
+
+# delete multiple/all records
+programmers = session.query(Programmer)
+for programmer in programmers:
+    session.delete(programmer)
+    session.commit()
+
 
 # query the database to find all Programmers
 programmers = session.query(Programmer)
