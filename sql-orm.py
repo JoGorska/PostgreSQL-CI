dec09beb72f980d81a8b955e69ada91094253e00
Joanna Gorska <gorska919@gmail.com>
2021-11-21 17:43:07 +0000

    Add queries to sql-orm.py

--- sql-orm.py
+++ sql-orm.py
@@ -13,7 +13,7 @@ from sqlalchemy.orm import sessionmaker
 # execute instructions from "chinook" database
 
 db = create_engine("postgresql:///chinook")
-base = declarative_base
+base = declarative_base()
 
 
 # create a class-based model for the "Artist" table
@@ -53,3 +53,45 @@ Session = sessionmaker(db)
 # opens instance, by calling the Session() subclass created above
 
 session = Session()
+
+# creating the database using declarative_base subclass
+base.metadata.create_all(db)
+
+# Query 1 - select all records from the "Artist" table
+artists = session.query(Artist)
+for artist in artists:
+    print(artist.ArtistId, artist.Name, sep=" | ")
+
+# Query 2 - select only the "Name" column from the "Artist" table
+# artists = session.query(Artist)
+# for artist in artists:
+#     print(artist.Name)
+
+# Query 3 - select only "Queen" from the "Artist" table
+# artist = session.query(Artist).filter_by(Name="Queen").first()
+# print(artist.ArtistId, artist.Name, sep=" | ")
+
+# Query 4 - select only by "ArtistId" #51 from the "Artist" table
+# artist = session.query(Artist).filter_by(ArtistId=51).first()
+# print(artist.ArtistId, artist.Name, sep=" | ")
+
+# Query 5 - select only the albums with "ArtistId" #51 on the "Album" table
+# albums = session.query(Album).filter_by(ArtistId=51)
+# for album in albums:
+#     print(album.AlbumId, album.Title, album.ArtistId, sep=" | ")
+
+# Query 6 - select all tracks where the composer is "Queen" from the "Track" table
+# tracks = session.query(Track).filter_by(Composer="Queen")
+# for track in tracks:
+#     print(
+#         track.TrackId,
+#         track.Name,
+#         track.AlbumId,
+#         track.MediaTypeId,
+#         track.GenreId,
+#         track.Composer,
+#         track.Milliseconds,
+#         track.Bytes,
+#         track.UnitPrice,
+#         sep=" | "
+#     )
