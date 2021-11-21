daa2ae25c4c97e3131d278270d78641a19900d97
Joanna Gorska <gorska919@gmail.com>
2021-11-21 19:50:39 +0000

    Add code to delete a single record from the database

--- sql-crud.py
+++ sql-crud.py
@@ -112,16 +112,35 @@ gal_anonim = Programmer(
 # session.commit()
 
 
-# updating multiple records
-people = session.query(Programmer)
-for person in people:
-    if person.gender == "F":
-        person.gender = "Female"
-    elif person.gender == "M":
-        person.gender = "Male"
+# # updating multiple records
+# people = session.query(Programmer)
+# for person in people:
+#     if person.gender == "F":
+#         person.gender = "Female"
+#     elif person.gender == "M":
+#         person.gender = "Male"
+#     else:
+#         print("Gender not defined")
+#     session.commit()
+
+
+# deleting a single record
+fname = input("Enter a first name: ")
+lname = input("Enter a last name: ")
+programmer = session.query(Programmer).filter_by(first_name=fname, last_name=lname).first()
+
+# defensive programming
+if programmer is not None:
+    print("Programmer Found: ", programmer.first_name + " " + programmer.last_name)
+    confirmation = input("Are you sure you want to delete this record? (y/n) ")
+    if confirmation.lower() == "y":
+        session.delete(programmer)
+        session.commit()
+        print("Programmer has been deleted")
     else:
-        print("Gender not defined")
-    session.commit()
+        print("Programmer not deleted")
+else:
+    print("No records found")
 
 # query the database to find all Programmers
 programmers = session.query(Programmer)
