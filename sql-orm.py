3752d326b69878ab9754d239e82e6c361fe8f3f9
Joanna Gorska <gorska919@gmail.com>
2021-11-21 18:00:40 +0000

    Add initial setup for sql-crud.py file and add programers

--- sql-orm.py
+++ sql-orm.py
@@ -80,7 +80,8 @@ for artist in artists:
 # for album in albums:
 #     print(album.AlbumId, album.Title, album.ArtistId, sep=" | ")
 
-# Query 6 - select all tracks where the composer is "Queen" from the "Track" table
+# Query 6 - select all tracks where the composer is "Queen" 
+# from the "Track" table
 # tracks = session.query(Track).filter_by(Composer="Queen")
 # for track in tracks:
 #     print(
